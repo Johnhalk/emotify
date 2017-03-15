c56e127a5002f98d12182d9f9294a0f2f67e1c0e
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-03-15 14:44:41 +0000

    Refactored Upload component
    upload component is now a stateless object which
    simply calls the emotion API and passes the file octet
    through as a prop

--- src/components/upload.js
+++ src/components/upload.js
@@ -2,38 +2,17 @@ import React, { Component } from 'react';
 import {callAPI} from '../services/emotion'
 
 class Upload extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      ip: 'Loading',
-      fileObj: null,
-    };
-  }
-
   handleFileUpload = () => {
     const file = this.refs.fileName.files[0];
-    this.setState({fileObj: file})
-    this.fetchAjax(file);
-  }
-
-  fetchAjax = (file) => {
-  console.log('Fetching ajax...');
-  callAPI(file)
-    .then(ip => this.setState({ip: ip}))
-    .catch(err => console.log(err, 'There was an error'))
+    callAPI(file);
   }
 
   render() {
-    let {ip} = this.state;
     return (
-      <div>
       <form>
       <input type="file" ref="fileName" name="fileName" />
       <button type='button' onClick={this.handleFileUpload}>Upload</button>
       </form>
-      {ip}
-      </div>
     );
   }
 }
