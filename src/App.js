a3d3081ce4194b448468e6bd88b705593686c2ec
Enrico Graziani <mrenrich84@gmail.com>
2017-03-15 11:21:38 +0000

    Finally Graph is working, missing tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Graph from './components/graph/graph.js';
+import DataConverterContainer from './components/graph/graph.js';
 
 var jsonData = JSON.parse('[{"faceRectangle":{"height":64,"left":85,"top":50,"width":64},"scores":{"anger":0.000008362581,"contempt":0.00023066772,"disgust":0.00000190693913,"fear":0.00000262125877,"happiness":0.0000301946984,"neutral":0.000008362581,"sadness":0.000635955657,"surprise":0.0000298200466}}]');
 
@@ -16,7 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Graph jsonData={jsonData}/>
+        <DataConverterContainer jsonData={jsonData}/>
       </div>
     );
   }
--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -1,40 +1,43 @@
 import React, { Component } from 'react';
-import RadarChart, { Radar } from 'react-chartjs';
-// var RadarChart = require("react-chartjs").Radar;
+// import RadarChart, { Radar } from 'react-chartjs';
 
+var RadarChart = require("react-chartjs").Radar;
 
-class Graph extends Component {
-  constructor(jsonData){
-    super();
-    this.setState( { dataset: this.getDatasetfromJsonData(this.props.jsonData)})
-  }
-  
+class DataConverterContainer extends Component {
   getDatasetfromJsonData(jsonData){
     return jsonData[0].scores;
   };
-  //
-  // getLabels(){
-  //   return Object.keys(this.state.dataset)
-  // };
-  //
-  // getData(){
-  //   // probably need to loop through each label to get values in the proper order
-  //   return Object.values(this.state.dataset)
-  // };
-  //
-  // chartData(){
-  //     return {
-  //       labels: this.getLabels(),
-  //       datasets: [{
-  //           data: this.getData()
-  //       }]
-  //     }
-  // };
+
+  getLabels(dataset){
+    return Object.keys(dataset)
+  };
+
+  getData(dataset){
+    // probably need to loop through each label to get values in the proper order
+    return Object.values(dataset)
+  };
+
+  chartData = (dataset) => {
+      return {
+        labels: this.getLabels(dataset),
+        datasets: [{
+            data: this.getData(dataset)
+        }]
+      }
+  };
+
 
   render() {
-    return <p>{JSON.stringify(this.props.jsonData)}</p>
-    // return <RadarChart data={this.chartData()} width={600} height={700}/>
+    var dataset = this.getDatasetfromJsonData(this.props.jsonData);
+    let chartData = this.chartData(dataset)
+    return <Graph chartData={chartData} width={800} height={800}/>
   }
 };
 
-export default Graph;
+
+
+const Graph = ({chartData, width, height}) => {
+  return <RadarChart data={chartData} width={width} height={height} />
+};
+
+export default DataConverterContainer;
