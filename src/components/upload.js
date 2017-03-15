36ad535757305a6d052cd91b82f831c737cb38b7
Enrico Graziani <mrenrich84@gmail.com>
2017-03-15 17:08:53 +0000

    Making master works and writing some tests for JSONDataConverter

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import { callAPI } from '../services/emotion';
+import GraphPresentation from './graph/graphPresentation'
+
 
 class Upload extends Component {
 
   constructor(){
     super()
     this.state = {
-      data: 'Awaiting input'
+      faceData: 'Awaiting input'
     };
   }
 
   handleFileUpload = () => {
     const file = this.refs.fileName.files[0];
     callAPI(file)
-    .then(data => this.setState({data: data}))
+    .then(faceData => this.setState({faceData: faceData}))
     .catch(err => console.log(err, 'There was an error'))
   }
 
   render() {
-    let {data} = this.state;
+    let {faceData} = this.state;
+    if (faceData !== 'Awaiting input') {
+       var graphPresentation = <GraphPresentation data={JSON.parse(faceData)} width={600} height={600}  />
+    };
+
     return (
       <div>
       <form>
       <input type="file" ref="fileName" name="fileName" />
       <button type='button' onClick={this.handleFileUpload}>Upload</button>
       </form>
-      {data}
+        {graphPresentation}
       </div>
     );
   }
