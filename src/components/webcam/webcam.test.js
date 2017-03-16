8f97b6ccca94c293881d9fbd7f3b5a78d0c73039
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-03-16 16:31:44 +0000

    Added canvas dependency to master branch for unit testing

---
+++ src/components/webcam/webcam.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow, mount, render} from 'enzyme';
+import Webcam from './webcam'
+
+describe('Webcam container', () => {
+
+});
