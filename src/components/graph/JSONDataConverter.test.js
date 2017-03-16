081d66aaa69101a303b1918ecbe268b2700d2f6e
John Ashton <John_ashton@hotmail.com>
2017-03-16 15:12:41 +0000

    mvp refacotring

--- src/components/graph/JSONDataConverter.test.js
+++ src/components/graph/JSONDataConverter.test.js
@@ -1,17 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { shallow, mount, render } from 'enzyme';
 var JSONDataConverter = require('./JSONDataConverter');
 
 describe('JSONDataConverter', () => {
   it('converts an MCS Emotion API JSON response to a ChartJS data object', () => {
     var jsonData = JSON.parse('[{"faceRectangle":{"height":64,"left":85,"top":50,"width":64},"scores":{"anger":0.000008362581,"contempt":0.00023066772,"disgust":0.00000190693913,"fear":0.00000262125877,"happiness":0.0000301946984,"neutral":0.000008362581,"sadness":0.000635955657,"surprise":0.0000298200466}}]');
-    var dataObj = JSONDataConverter(jsonData);
-    // store the expectedObj
-    expect(dataObj.labels).toContain('anger'); // test that dataObj === expectedObj 
+    var expectedObj = JSON.parse('{"labels":["anger","contempt","disgust","fear","happiness","neutral","sadness","surprise"],"datasets":[{"data":[0.000008362581,0.00023066772,0.00000190693913,0.00000262125877,0.0000301946984,0.000008362581,0.000635955657,0.0000298200466]}]}')
+    expect(JSONDataConverter(jsonData)).toEqual(expectedObj);
   });
 
-  it('fails if there is no argument', () => {
+ it('fails if there is no argument', () => {
     expect(function(){JSONDataConverter()}).toThrow('Cannot convert data: missing JSON data object');
   });
 });
---
+++ src/services/emotion.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
+import { callAPI } from '../services/emotion.js'
+
+
+describe('callAPI', function(){
+
+  beforeEach(function() {
+    global.fetch = jest.fn().mockImplementation(() => {
+      var p = new Promise((resolve, reject) => {
+        resolve({
+          ok: true,
+          Id: '123',
+          json: function() {
+            return {Id: '123'}
+          }
+        });
+      });
+      return p;
+    });
+  });
+
+  it("callAPI", async function() {
+    const response = await callAPI('foo');
+    expect(response).toBe("{\"Id\":\"123\"}");
+  });
+});
