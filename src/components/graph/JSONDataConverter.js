ff4f1a2fa426ff574063bd61a290b78be5c8a45f
Sam Mckay <sammckay12@gmail.com>
2017-03-17 14:53:38 +0000

    multi face displays average graph

--- src/components/graph/JSONDataConverter.js
+++ src/components/graph/JSONDataConverter.js
@@ -1,7 +1,3 @@
-function getDatasetfromJsonData(jsonData){
-  return jsonData[0].scores;
-};
-
 function getLabels(dataset) {
   return Object.keys(dataset).sort()
 };
@@ -26,6 +22,44 @@ function checkForErrors(JSONData) {
     throw('Cannot convert data: missing JSON data object');
   }
 };
+
+function getDatasetfromJsonData (dataset) {
+   var accumulatedEmotions = {
+     totalAnger: 0,
+     totalContempt: 0,
+     totalDisgust: 0,
+     totalFear: 0,
+     totalHappiness: 0,
+     totalNeutral: 0,
+     totalSadness: 0,
+     totalSurprise: 0,
+   };
+
+   dataset.forEach(function(object, index) {
+     if (index !== dataset.length) {
+       accumulatedEmotions.totalAnger += object.scores.anger
+       accumulatedEmotions.totalContempt += object.scores.contempt
+       accumulatedEmotions.totalDisgust += object.scores.disgust
+       accumulatedEmotions.totalFear += object.scores.fear
+       accumulatedEmotions.totalHappiness += object.scores.happiness
+       accumulatedEmotions.totalNeutral += object.scores.neutral
+       accumulatedEmotions.totalSadness += object.scores.sadness
+       accumulatedEmotions.totalSurprise += object.scores.surprise
+     }
+
+   });
+   var averagedEmotions = {
+     anger:  accumulatedEmotions.totalAnger / dataset.length,
+     contempt:  accumulatedEmotions.totalContempt / dataset.length,
+     disgust:  accumulatedEmotions.totalDisgust / dataset.length,
+     fear:  accumulatedEmotions.totalFear / dataset.length,
+     happiness:  accumulatedEmotions.totalHappiness / dataset.length,
+     neutral:  accumulatedEmotions.totalNeutral / dataset.length,
+     sadnes:  accumulatedEmotions.totalSadness / dataset.length,
+     surprise:  accumulatedEmotions.totalSurprise / dataset.length,
+   }
+   return averagedEmotions
+ }
 
 module.exports = function(JSONdata) {
   checkForErrors(JSONdata)
