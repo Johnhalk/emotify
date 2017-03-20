b287442068ddaf29a35fe26209c4f460637ec2c2
Enrico Graziani <mrenrich84@gmail.com>
2017-03-20 16:04:34 +0000

    Installing react-swipeable and testing - it works

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SnapshotContainer from './components/snapshot/snapshotContainer'
 import GraphContainer from './components/graph/graphContainer'
 var  mcsEmotionApiTalker = require ('./services/mcsEmotionApiTalker');
+import SwipeableViews from 'react-swipeable-views';
 
 class App extends Component {
   constructor(){
@@ -45,6 +46,22 @@ class App extends Component {
 
   render() {
     const isMobile = this.state.windowWidth <= 500;
+    const styles = {
+      slide: {
+        padding: 15,
+        color: '#fff',
+        minHeight: 100,
+      },
+      slide1: {
+        background: '#FEA900',
+      },
+      slide2: {
+        background: '#B3DC4A',
+      },
+      slide3: {
+        background: '#6AC0FF',
+      },
+    };
 
     let {faceData} = this.state;
     if (faceData !== 'Awaiting input...') {
@@ -58,19 +75,37 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <p className="App-intro">
-          Choose a file and click analyse to begin!
-        </p>
-        <div>
-          <SnapshotContainer onChange={this.getEmotionData} interval={this.state.interval} />
-        </div>
-          {graphContainer}
+        <SwipeableViews>
+          <div style={Object.assign({}, styles.slide, styles.slide1)}>
+            <SnapshotContainer onChange={this.getEmotionData} interval={this.state.interval} />
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide2)}>
+            slide n°2
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide3)}>
+            slide n°3
+          </div>
+        </SwipeableViews>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
       </div>
     )
 
     var mobile_version = (
       <div className="App">
-MOBILE
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <SwipeableViews>
+          <div style={Object.assign({}, styles.slide, styles.slide1)}>
+            <SnapshotContainer onChange={this.getEmotionData} interval={this.state.interval} />
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide2)}>
+            slide n°2
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide3)}>
+            slide n°3
+          </div>
+        </SwipeableViews>
       </div>
     )
 
