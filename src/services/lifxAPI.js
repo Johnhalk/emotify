d7d2365e07639893b5b7c5b13f08b354e5f6ba52
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-03-21 18:16:33 +0000

    ENV

--- src/services/lifxAPI.js
+++ src/services/lifxAPI.js
@@ -11,7 +11,7 @@ export function updateColour(colour) {
     }),
     headers: new Headers({
       "Accept": "*/*",
-      "Authorization" : "Bearer c19dd2b0a1fe3916afbc45116b6b153dac02e5c784c31a5e5cc0900b60dbe8fd",
+      "Authorization" : process.env.REACT_APP_LIFX_SECTRET,
       "Content-Type": "application/json",
       "Content-Length": 56,
       "Accept-Encoding": "gzip, deflate"
@@ -28,7 +28,7 @@ export function togglePower() {
     method: 'POST',
     mode: 'cors',
     headers: new Headers({
-      "Authorization" : "Bearer c19dd2b0a1fe3916afbc45116b6b153dac02e5c784c31a5e5cc0900b60dbe8fd"
+      "Authorization" : process.env.REACT_APP_LIFX_SECTRET
     })
   })
   return fetch(request)
