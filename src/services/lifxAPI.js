50a4dda81df6cc25b56d95d4e9b7b2d02ac42b58
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-03-21 18:35:56 +0000

    Bearer string from LIFXAPI

--- src/services/lifxAPI.js
+++ src/services/lifxAPI.js
@@ -11,7 +11,7 @@ export function updateColour(colour) {
     }),
     headers: new Headers({
       "Accept": "*/*",
-      "Authorization" : process.env.REACT_APP_LIFX_SECTRET,
+      "Authorization" : 'Bearer ' + process.env.REACT_APP_LIFX_SECTRET,
       "Content-Type": "application/json",
       "Content-Length": 56,
       "Accept-Encoding": "gzip, deflate"
@@ -28,7 +28,7 @@ export function togglePower() {
     method: 'POST',
     mode: 'cors',
     headers: new Headers({
-      "Authorization" : process.env.REACT_APP_LIFX_SECTRET
+      "Authorization" : 'Bearer ' + process.env.REACT_APP_LIFX_SECTRET
     })
   })
   return fetch(request)
