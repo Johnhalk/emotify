9250a688442671f1de5903d7e9281e35ac565fa4
Enrico Graziani <mrenrich84@gmail.com>
2017-03-21 18:47:54 +0000

    Finished rebasing to master

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import './App.css';
 import SnapshotContainer from './components/snapshot/snapshotContainer'
 import GraphContainer from './components/graph/graphContainer'
 import ColoursContainer from './components/colours/coloursContainer'
-import {callAPI} from './services/mcsEmotionApiTalker'
-import ColorsConverter from './components/colors/colorsConverter';
 
 import SwipeableViews from 'react-swipeable-views';
 
@@ -57,45 +55,30 @@ class App extends Component {
     let {faceData} = this.state;
 
     var graphContainer
-    var backgroundColor
     if (faceData !== 'Awaiting input...') {
        graphContainer = <GraphContainer data={faceData} width={200} height={200} interval={this.state.interval} />
+       var colourContainer = <ColoursContainer data={faceData}/>
 
-       var color = ColorsConverter(faceData)
-       backgroundColor = {
-         backgroundColor: `hsl(${color.hue}, ${color.saturation}%,  ${color.lightness}%)`
-       }
     } else {
       graphContainer = faceData
-      backgroundColor = {
-        backgroundColor: `hsl(50, 0%,  0%)`
-      }
+      var colourContainer;
     }
 
     const { tabIndex } = this.props;
     return (
       <div className="App">
-        <MuiThemeProvider muiTheme={getMuiTheme()}>
-          <div className="main-cointaner">
-            <Tabs value={index}>
-              <Tab label="PANEL" value={0} onClick={this.handleChangeTabs(0)} />
-              <Tab label="GRAPH" value={1} onClick={this.handleChangeTabs(1)} />
-              <Tab label="COLOR" value={2} onClick={this.handleChangeTabs(2)} />
-            </Tabs>
-            <SwipeableViews index={index} onChangeIndex={this.handleChangeIndex} className="swipeable-views">
-              <div style={Object.assign({}, styles.slide, styles.slide1)}>
-                <SnapshotContainer onChange={this.getEmotionData} interval={this.state.interval} />
-              </div>
-              <div style={Object.assign({}, styles.slide, styles.slide2)}>
-                {graphContainer}
-              </div>
-              <div style={Object.assign({}, styles.slide, styles.slide3, backgroundColor)} className="asadsafd">
-                SEE CHANGING COLORS!
-                {colourContainer}
-              </div>
-            </SwipeableViews>
+        <SwipeableViews index={this.props.tabIndex} onChangeIndex={this.handleChangeIndex} className="swipeable-views">
+          <div style={Object.assign({}, styles.slide, styles.slide1)}>
+            <SnapshotContainer onChange={this.getEmotionData} interval={this.state.interval} />
           </div>
-        </MuiThemeProvider>
+          <div style={Object.assign({}, styles.slide, styles.slide2)}>
+            {graphContainer}
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide3)} className="asadsafd">
+            SEE CHANGING COLORS!
+            {colourContainer}
+          </div>
+        </SwipeableViews>
       </div>
     );
   }
