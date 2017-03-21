9dbde7edc27536b1cc4fade772a0f764af747d40
Enrico Graziani <mrenrich84@gmail.com>
2017-03-21 16:25:20 +0000

    Adding react-swipeable for swipe support and material-ui as ui library

--- src/App.js
+++ src/App.js
@@ -5,6 +5,33 @@ import SnapshotContainer from './components/snapshot/snapshotContainer'
 import GraphContainer from './components/graph/graphContainer'
 import ColoursContainer from './components/colours/coloursContainer'
 import {callAPI} from './services/mcsEmotionApiTalker'
+import ColorsConverter from './components/colors/colorsConverter';
+
+// bunch of needed material-ui components
+import Tabs from 'material-ui/Tabs/Tabs';
+import Tab from 'material-ui/Tabs/Tab';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Checkbox from 'material-ui/Checkbox';
+import SwipeableViews from 'react-swipeable-views';
+
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+    height: "100%"
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#6AC0FF',
+  },
+};
 
 class App extends Component {
   constructor(){
@@ -13,9 +40,49 @@ class App extends Component {
       faceData: 'Awaiting input...',
       interval: 4000,
       height: 600,
-      width: 600
+      width: 600,
+      windowWidth: window.innerWidth,
+      index: 0,
     }
   }
+
+  handleChangeTabs = (value) => () => {
+    this.setState({
+      index: value,
+    });
+  };
+
+  handleChangeIndex = (index) => {
+    this.setState({
+      index,
+    });
+  };
+
+  setIsMobile = () => {
+    this.setState({
+      isMobile: this.state.windowWidth <= 500
+    });
+  };
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+    this.setIsMobile()
+
+  }
+
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({
+      windowWidth: window.innerWidth
+     });
+    this.setIsMobile()
+  };
+
 
   updateFaceData = (faceData) => {
     this.setState({
@@ -30,13 +97,21 @@ class App extends Component {
   }
 
   render() {
+    const { index } = this.state;
+
     let {faceData} = this.state;
     if (faceData !== 'Awaiting input...') {
-       var graphContainer = <GraphContainer data={faceData} width={this.state.width} height={this.state.height} interval={this.state.interval} />
-       var colourContainer = <ColoursContainer data={faceData}/>
+       var graphContainer = <GraphContainer data={faceData} width={200} height={200} interval={this.state.interval} />
+
+       var color = ColorsConverter(faceData)
+       var backgroundColor = {
+         backgroundColor: `hsl(${color.hue}, ${color.saturation}%,  ${color.lightness}%)`
+       }
     } else {
       var graphContainer = faceData
-      var colourContainer;
+      var backgroundColor = {
+        backgroundColor: `hsl(50, 0%,  0%)`
+      }
     }
 
     return (
@@ -44,15 +119,27 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <p className="App-intro">
-          Choose a file and click analyse to begin!
-        </p>
-        <div>
-          <SnapshotContainer onChange={this.getEmotionData} interval={this.state.interval} />
-          {colourContainer}
-        </div>
-        <br/>
-          {graphContainer}
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <div className="main-cointaner">
+            <Tabs value={index}>
+              <Tab label="PANEL" value={0} onClick={this.handleChangeTabs(0)} />
+              <Tab label="GRAPH" value={1} onClick={this.handleChangeTabs(1)} />
+              <Tab label="COLOR" value={2} onClick={this.handleChangeTabs(2)} />
+            </Tabs>
+            <SwipeableViews index={index} onChangeIndex={this.handleChangeIndex} className="swipeable-views">
+              <div style={Object.assign({}, styles.slide, styles.slide1)}>
+                <SnapshotContainer onChange={this.getEmotionData} interval={this.state.interval} />
+              </div>
+              <div style={Object.assign({}, styles.slide, styles.slide2)}>
+                {graphContainer}
+              </div>
+              <div style={Object.assign({}, styles.slide, styles.slide3, backgroundColor)} className="asadsafd">
+                SEE CHANGING COLORS!
+                {colourContainer}
+              </div>
+            </SwipeableViews>
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
