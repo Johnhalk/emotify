1194c7286a50fce32e36dc609487bf1673f8eb7e
Enrico Graziani <mrenrich84@gmail.com>
2017-03-22 14:59:22 +0000

    Removing everything again - can use this as a Bootstrap base version

--- src/AppBody.js
+++ src/AppBody.js
@@ -7,39 +7,10 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
-import Modal from 'react-bootstrap/lib/Modal';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
 
 import SnapshotContainer from './components/snapshot/snapshotContainer'
 import GraphContainer from './components/graph/graphContainer'
 import ColoursContainer from './components/colours/coloursContainer'
-
-
-// const {
-//   Button,
-//   Modal,
-//   Nav,
-//   NavItem
-// } = ReactBootstrap;
-
-class Sidebar extends React.Component {
-	render() {
-  	return (
-    	<Modal className='Sidebar left' show={ this.props.isVisible } onHide={this.props.onHide}
-      	 autoFocus keyboard
-      >
-      	<Modal.Header closeButton>
-        	<Modal.Title>Sidebar Menu</Modal.Title>
-        </Modal.Header>
-      	<Modal.Body>
-      		{ this.props.children }
-        </Modal.Body>
-      </Modal>
-    );
-  }
-}
-
 
 
 class AppBody extends Component {
@@ -50,15 +21,8 @@ class AppBody extends Component {
       interval: 4000,
       height: 600,
       width: 600,
-      index: 0,
-      isVisible: false,
     };
   }
-
-    updateModal(isVisible) {
-    	this.state.isVisible = isVisible;
-      this.forceUpdate();
-    }
 
   updateFaceData = (faceData) => {
     this.setState({
@@ -96,18 +60,6 @@ class AppBody extends Component {
 
     return (
       <div className="AppBody">
-        <div className='Sidebar-demo'>
-          <Button onClick={ () => this.updateModal(true) }>Display Modal Dialog</Button>
-          <Sidebar side='left' isVisible={ this.state.isVisible } onHide={ () => this.updateModal(false) }>
-            <Nav>
-              <NavItem href='#'>Item 1</NavItem>
-              <NavItem href='#'>Item 2</NavItem>
-              <NavItem href='#'>Item 3</NavItem>
-              <NavItem href='#'>Item 4</NavItem>
-              <NavItem href='#'>Item 5</NavItem>
-            </Nav>
-          </Sidebar>
-        </div>
         <Grid className="container-fluid">
           <Row className="show-grid">
             <Col xs={6} md={4}>
